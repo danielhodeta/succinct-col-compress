b67426f0f64398c75f51aa2ad6b43b3c0d3249b6
danielhodeta <danielhodeta@users.noreply.github.com>
2020-04-25 22:17:37 -0400

    Added bash scripts for building and cleanup

---
+++ cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -f -r CMakeFiles
+rm -f cmake_install.cmake
+rm -f Makefile
+
+rm -f -r src/CMakeFiles
+rm -f src/cmake_install.cmake
+rm -f src/Makefile
+
+rm -f CMakeCache.txt
+
+rm -f -r build
