2f8939565fd676e10e2cad647187f716a04c43cd
danielhodeta <danielhodeta@users.noreply.github.com>
2020-04-26 22:31:32 -0400

    Modified cleanup scripts

--- cleanup.sh
+++ cleanup.sh
@@ -5,15 +5,19 @@
 rm -f -r CMakeFiles
 rm -f cmake_install.cmake
 rm -f Makefile
+rm -f CMakeCache.txt
+echo "cleaned cmake files in parent directory"
 
 rm -f -r src/CMakeFiles
 rm -f src/cmake_install.cmake
 rm -f src/Makefile
+echo "cleaned cmake files in src directory"
 
-rm -f CMakeCache.txt
+rm -f -r build
+echo "cleaned build"
 
-rm -f -r build/
+rm -f -r bin
+echo "cleaned bin"
 
-rm -f -r bin/
-
-rm -f -r out/
+rm -f -r out
+echo "cleaned out"
