3cae039b6f98a7c638ac8171469d6318385ca6f6
danielhodeta <danielhodeta@users.noreply.github.com>
2020-05-28 13:49:23 -0400

    Splitting into all cols (& compressing) in one run

--- src/col_compress.cpp
+++ src/col_compress.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <fstream>
-#include "CompressCols.h"
+#include "compress_cols.h"
 
 void display_error(std::string msg) {
     std::cout<<"col_compress: "<<msg<<'\n';
@@ -20,26 +20,29 @@ int main(int argc, char **argv) {
 
     char *file_path {argv[1]};
     std::string f_path {file_path};                                         //Converted to C++ String to allow concatenation
-    
 
-    int col_num = atoi(argv[2]);                                            //Indicate which column you would like to save
-    if (col_num<1) display_error("column number must be at least 1");
-    
-    auto *cfile = new CompressCols(f_path, col_num);
+    int total_col_num = atoi(argv[2]);                                      //Indicate total col number
+    if (total_col_num<1) display_error("total column number must be at least 1");
+    CompressCols::Split(f_path, 12);
+    for (int i=1; i<=total_col_num; i++) {
+        
+        auto *cfile = new CompressCols(f_path, total_col_num, i);
 
-    for (int i=3; i<argc; i++) {
-        std::string scheme = argv[i];
+        for (int j=3; j<argc; j++) {
+            std::string scheme = argv[j];
+            
+                if(cfile->Compress(scheme))
+                    std::cout<<"compressed column "+std::to_string(i)+" using "+scheme+".\n";
+                else
+                    std::cout<<scheme + " compression unsuccessful for column "+std::to_string(i)+".\n";
+                // if(cfile->Decompress())
+                //     std::cout<<scheme + " decompression successful for column "+std::to_string(i)+".\n";
+                // else
+                //     std::cout<<scheme + " decompression unsuccessful for column "+std::to_string(i)+".\n";
 
-        if(cfile->Compress(scheme))
-            std::cout<<"compressed column "+std::to_string(col_num)+" using "+scheme+".\n";
-        else
-            std::cout<<scheme + " compression unsuccessful for column "+std::to_string(col_num)+".\n";
+        }
 
-        if(cfile->Decompress())
-            std::cout<<scheme + " decompression successful for column "+std::to_string(col_num)+".\n";
-        else
-            std::cout<<scheme + " decompression unsuccessful for column "+std::to_string(col_num)+".\n";
-
+        delete cfile;
     }
 
     
